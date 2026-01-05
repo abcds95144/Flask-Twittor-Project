e2d93952e680b00c36d59a162297175a8241030b
Joshua Chang <abcds95144@gmail.com>
2026-01-05 23:45:30 +0800

    SQLAlchemy的配置與model

--- twittor/__init__.py
+++ twittor/__init__.py
@@ -1,16 +1,25 @@
 import os
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 from twittor.route import index, login
 from dotenv import load_dotenv
 
+load_dotenv()
 
-load_dotenv()  # 讀取 .env
+db = SQLAlchemy()
+migrate = Migrate()
 
 def create_app():
-
-    app=Flask(__name__)
-
+    app = Flask(__name__)
+    # ✅ 修正資料庫 URI
+    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///twittor.db"
     app.config['SECRET_KEY'] = os.getenv('SECRET_KEY') or 'fallback-secret-key'
-    app.add_url_rule('/index','index',index)
-    app.add_url_rule('/login','login',login, methods=['GET','POST'])
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    app.add_url_rule('/index', 'index', index)
+    app.add_url_rule('/login', 'login', login, methods=['GET','POST'])
+
     return app
