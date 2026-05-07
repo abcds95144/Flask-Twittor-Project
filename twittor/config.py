bd4290d4fdf981b3511ca95156b6543ba88485a3
Joshua Chang <abcds95144@gmail.com>
2026-05-08 04:46:11 +0800

    增加個人頁面及主頁分頁

--- twittor/config.py
+++ twittor/config.py
@@ -6,3 +6,4 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL", "sqlite:///" + os.path.join(config_path, 'twittor.db'))
     SECRET_KEY = os.getenv('SECRET_KEY') or 'fallback-secret-key'
     SECRET_KEY = 'abc123'
+    TWEET_PER_PAGE = 20
